b3f7402e75a049307e55c50d66fd114ad5175717
Azat0201 <101017303+Azat0201@users.noreply.github.com>
2023-09-07 18:57:17 +0300

    Commit

--- venv/Game.py
+++ venv/Game.py
@@ -1,4 +1,83 @@
 import random
 import pygame
 
-print(1)
+
+WIDTH = 1200
+HEIGTH = 707
+SCREEN_SIZE = (WIDTH, HEIGTH)
+IMAGE_WIDTH = 400
+IMAGE_HEIGTH = 400
+IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGTH)
+FPS = 30
+
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+ORANGE = (255, 150, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+LIGTH_BLUE = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (255, 0, 255)
+COLORS = ()
+for color in (RED, ORANGE, YELLOW, GREEN, LIGTH_BLUE, BLUE, PURPLE):
+    COLORS += (color, )
+
+pygame.init()
+#pygame.mixer.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+background = pygame.Surface(SCREEN_SIZE)
+background.fill(pygame.Color(BLACK))
+pygame.display.set_caption("Game")
+clock = pygame.time.Clock()
+
+image = pygame.image.load("ball.jpg")
+image = pygame.transform.scale(image, IMAGE_SIZE)
+screen.blit(image, (0, 0))
+
+class Butoon:
+    def __init__(self, x, y, title="", width=150, heigt=75, font='Verdana', size_font=12, button_color=GRAY, text_color=BLACK):
+        self.font = pygame.font.SysFont(font, size_font)
+        self.button_color = button_color
+        self.text_color = text_color
+        self.width = width
+        self.heigth = heigt
+        self.rect = pygame.Rect(x - width // 2, y - heigt // 2, width, heigt)
+        self.text = self.font.render(title, True, text_color)
+
+    def get_rect_center(self):
+        return self.text.get_rect(center=self.rect.center)
+
+button1 = Butoon(WIDTH // 2 - 100, HEIGTH // 2, title='Вперёд!')
+button2 = Butoon(WIDTH // 2 + 100, HEIGTH // 2, title='Назад!')
+
+color = 0
+while True:
+    screen.fill(COLORS[color % len(COLORS)])
+    clock.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                color += 1
+            elif event.key == pygame.K_s:
+                color -= 1
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if button1.rect.collidepoint(event.pos):
+                color += 1
+            if button2.rect.collidepoint(event.pos):
+                color -= 1
+
+    screen.blit(image, (WIDTH // 2 - IMAGE_WIDTH // 2, HEIGTH // 2 - IMAGE_HEIGTH // 2))
+
+    pygame.draw.rect(screen, button1.button_color, button1.rect)
+    screen.blit(button1.text, button1.get_rect_center())
+
+    pygame.draw.rect(screen, button2.button_color, button2.rect)
+    screen.blit(button2.text, button2.get_rect_center())
+
+    pygame.display.update()
---
+++ venv/Matrix.py
@@ -0,0 +1,100 @@
+import pygame
+import pygame_gui
+from random import randint, choice
+
+WINDOW_WIDTH = 1920
+WINDOW_HEIGTH = 1080
+WINDOW_SIZE = (WINDOW_WIDTH, WINDOW_HEIGTH)
+FPS = 100
+SURFACE_SIZE = 500
+window = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption('Матрица Lite')
+pygame.init()
+gui_manager = pygame_gui.UIManager(WINDOW_SIZE)
+
+TEXT_FONT = pygame.font.SysFont('Verdana', 15)
+TEXT_COLOR = pygame.Color('green')
+TEXT_SYMBOLS = '1234567890-=+_)(*&^%$#!"№;:?qwertyuiop[]|\\{}QWERTYUIOPйцукенгшщзхъЙЦУКЕНГШЩЗХЪ/asdfghjklASDFGHJKLфывапролджэФЫВАПРОЛДЖЭzxcvbnm,.ZXCVBNM<>ячсмитьбюЯЧСМИТЬБЮ'
+TEXT_POSITION = [(randint(1, WINDOW_WIDTH), randint(1, WINDOW_HEIGTH)) for i in range(SURFACE_SIZE)]
+NORMAL_TEXT_SPEED = tuple((0, randint(1, 100)) for i in range(SURFACE_SIZE))
+ZERO_TEXT_SPEED = tuple((0, 0) for _ in range(SURFACE_SIZE))
+TEXT_SPEED = NORMAL_TEXT_SPEED
+TEXT_SURFACE_LIST = []
+
+BUTTON_WIDTH = 100
+BUTTON_HEIGTH = 50
+BUTTON_SIZE = (BUTTON_WIDTH, BUTTON_HEIGTH)
+BUTTON_POSX = WINDOW_WIDTH // 2 - BUTTON_WIDTH // 2
+BUTTON_POSY = WINDOW_HEIGTH // 2 - BUTTON_HEIGTH // 2
+BUTTON1_TITLE = 'Матрица!'
+BUTTON2_TITLE = 'Пауза!'
+BUTTON3_TITLE = 'Выход!'
+
+
+class Butoon:
+    def __init__(self, x, y, title="", width=BUTTON_WIDTH, font=TEXT_FONT, heigt=BUTTON_HEIGTH, button_color='#606065', text_color='#FFFFFF'):
+        self.font = TEXT_FONT
+        self.button_color = button_color
+        self.text_color = text_color
+        self.width = width
+        self.heigth = heigt
+        self.rect = pygame.Rect(x, y, self.width, self.heigth)
+        self.title = self.font.render(title, True, self.text_color)
+
+    def get_rect_center(self):
+        return self.title.get_rect(center=self.rect.center)
+
+
+button1 = Butoon(BUTTON_POSX - 100, BUTTON_POSY, BUTTON1_TITLE, button_color='#000000', text_color='#000000')
+button2 = Butoon(BUTTON_POSX + 100, BUTTON_POSY, BUTTON2_TITLE, button_color='#000000', text_color='#000000')
+button3 = Butoon(BUTTON_POSX + 000, BUTTON_POSY, BUTTON3_TITLE, button_color='#000000', text_color='#000000')
+
+while True:
+    TIME_DELTA = pygame.time.Clock().tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if button1.rect.collidepoint(event.pos):
+                if TEXT_SURFACE_LIST:
+                    TEXT_SURFACE_LIST = []
+                else:
+                    for i in range(SURFACE_SIZE):
+                        text_symbol = choice(TEXT_SYMBOLS)
+                        TEXT_SURFACE_LIST.append(TEXT_FONT.render(text_symbol, True, TEXT_COLOR))
+            elif button2.rect.collidepoint(event.pos):
+                if TEXT_SPEED[0][1]:
+                    TEXT_SPEED = ZERO_TEXT_SPEED
+                else:
+                    TEXT_SPEED = NORMAL_TEXT_SPEED
+            elif button3.rect.collidepoint(event.pos):
+                pygame.quit()
+                quit()
+
+        gui_manager.process_events(event)
+
+    gui_manager.update(TIME_DELTA)
+
+    window.fill(pygame.Color('black'))
+
+    for i in range(SURFACE_SIZE):
+        TEXT_POSITION[i] = (TEXT_POSITION[i][0], TEXT_POSITION[i][1] + TEXT_SPEED[i][1])
+        if TEXT_POSITION[i][1] > WINDOW_SIZE[1]:
+            TEXT_POSITION[i] = (randint(0, WINDOW_SIZE[0]), -20)
+        if len(TEXT_SURFACE_LIST) > i:
+            window.blit(TEXT_SURFACE_LIST[i], TEXT_POSITION[i])
+
+    pygame.draw.rect(window, button1.button_color, button1.rect)
+    window.blit(button1.title, button1.get_rect_center())
+
+    pygame.draw.rect(window, button2.button_color, button2.rect)
+    window.blit(button2.title, button2.get_rect_center())
+
+    pygame.draw.rect(window, button3.button_color, button3.rect)
+    window.blit(button3.title, button3.get_rect_center())
+
+    gui_manager.draw_ui(window)
+    pygame.display.update()
